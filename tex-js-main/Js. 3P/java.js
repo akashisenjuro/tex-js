147fccb9d03f29d332f4f587cca74ddc7bcafbb1
Akashisenjuro <126684134+akashisenjuro@users.noreply.github.com>
2024-05-08 11:10:33 -0300

    Add files via upload

---
+++ tex-js-main/Js. 3P/java.js
@@ -0,0 +1,38 @@
+//Mostre um alerta com a mensagem "Bem-vindo ao nosso site!".
+alert("Bem vindo ao nosso site")
+
+//Declare uma variável chamada nome e atribua a ela o valor "Lua".
+let nome = "Lua";
+ console.log (nome)
+
+//Crie uma variável chamada ano de nascimento e atribua a ela o valor 1999.
+ let nascimento= "1999";
+ console.log (nascimento)
+
+//Defina uma variável numeroDeVendas e atribua a ela o valor 50.
+ let numeroDeVendas= "50";
+ console.log (numeroDeVendas)
+
+//Defina uma variável saldoDisponivel e atribua a ela o valor 1000.
+ let saldoDisponivel="1000"
+ console.log (saldoDisponivel)
+
+//Exiba um alerta com o texto "Erro! Preencha todos os campos."
+ alert("ERRO")
+
+
+//Declare uma variável chamada mensagemDeErro e atribua a ela o valor "Erro! Preencha todos os campos." Agora exiba um alerta com o valor da variável mensagemDeErro.
+ let mensagemDeErro= prompt("preencha tods os campos ")
+ 
+ //Use um prompt para perguntar o nome do usuário e armazená-lo na variável nome.
+ let lua = prompt("Qual é o seu nome?")
+ console.log (nome)
+
+ //Peça ao usuário para digitar sua idade usando um prompt e armazene-a na variável idade
+ let idade = prompt("Qual é a sua idade?")
+
+// Agora, caso a idade seja maior ou igual que 18, exiba um alerta com a mensagem "Pode tirar a habilitação!"
+idade = parseInt(idade);
+if (idade >= 18) {
+  alert("Pode tirar a habilitação!");
+}
